58fba917845b793d228a9a63902bd7f37c063b8b
github username <github email address>
2019-09-20 21:46:48 -0400

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,43 @@
+class SongsController < ApplicationController
+    
+    before_action :find_song, only: [:show, :edit, :update]
+
+    def index
+        @songs = Song.all
+    end
+
+    def new
+        @song = Song.new
+    end
+
+    def create
+        @song = Song.create(song_params)
+        redirect_to song_path(@song)
+    end
+
+    def show
+        @artist = @song.artist
+        @genre = @song.genre
+    end
+
+    def edit
+        @artists = Artist.all
+        @genres = Genre.all
+    end
+
+    def update
+        @song.update(song_params)
+        redirect_to song_path(@song)
+    end
+
+    private
+
+    def song_params
+        params.require(:song).permit(:name, :artist_id, :genre_id)
+    end
+
+    def find_song
+        @song = Song.find(params[:id])
+    end
+
+end
